b1789f71f1133a32453f26ff6a25ad5b97cd2d57
jai <jk8142727101@gmail.com>
2024-11-10 17:46:02 +0530

    jenkins

--- cicd/main.tf
+++ cicd/main.tf
@@ -30,6 +30,27 @@ module "jenkins_agent" {
   }
 }
 
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0ce45017043631db4"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-019801d0360b1b885"
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -52,6 +73,16 @@ module "records" {
       ttl     = 1
       records = [
         module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
       ]
       allow_overwrite = true
     }
